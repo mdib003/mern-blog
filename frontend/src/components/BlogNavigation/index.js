f675d2804dee69d708fe7220302584cf98dfcfa9
formex@2690 <mohammed.ibrahim3210@gmail.com>
2023-05-03 16:17:04 +0530

    blog navigation

---
+++ frontend/src/components/BlogNavigation/index.js
@@ -0,0 +1,15 @@
+import {Link } from "react-router-dom";
+
+
+export const BlogNavigation = () => {
+    return (
+        <div className="nav-container">
+            <div className="container">
+                <div className="flex justify-between align-center pad-t-16 pad-b-16">
+
+                </div>
+            </div>
+
+        </div>
+    )
+}
