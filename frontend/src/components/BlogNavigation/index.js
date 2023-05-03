72344abbfcd8a59dca5abd9184066dc3789f25dd
formex@2690 <mohammed.ibrahim3210@gmail.com>
2023-05-03 16:46:46 +0530

    blog css and global css

--- frontend/src/components/BlogNavigation/index.js
+++ frontend/src/components/BlogNavigation/index.js
@@ -6,7 +6,15 @@ export const BlogNavigation = () => {
         <div className="nav-container">
             <div className="container">
                 <div className="flex justify-between align-center pad-t-16 pad-b-16">
-
+                    <Link to='/' className="text-decoration-none color-white">MyBlog</Link>
+                    <ul className="flex">
+                        <li className="list-style-none mar-l-16">
+                            <Link to='/login' className="text-decoration-none color-white">Login</Link>
+                        </li>
+                        <li className="list-style-none mar-l-16">
+                            <Link to='/login' className="text-decoration-none color-white">Register</Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
 
